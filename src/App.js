a97a15dd2685d816a38432305e20ba23b7ab7546
vijaytembugade <vijaytembugade21@gmail.com>
2022-04-16 12:54:33 +0530

    private routes : user details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./Componets/Header/Header";
+import Login from "./Pages/Login/Login";
 import { VideoListing } from "./Pages/VideoListing/VideoListing";
+
+import Notify from "./Componets/Notify/Notify";
+import UserDetails from "./Pages/UserDetails/UserDetails";
+import PrivateRoutes from "./Componets/PrivateRoutes/PrivateRoutes";
+import Signup from "./Pages/Signup/Signup";
 
 function App() {
   return (
     <>
       <Header />
+      <Notify />
       <div className="main-container">
         <Routes>
           <Route path="/videos" element={<VideoListing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/user-details"
+            element={
+              <PrivateRoutes>
+                <UserDetails />
+              </PrivateRoutes>
+            }
+          />
         </Routes>
       </div>
     </>
